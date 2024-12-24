db8f35225273dfe198befd9eda35902f0d6f0814
kashif9598 <kashifsheikh9598@gmail.com>
2024-12-25 00:24:02 +0530

    Added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import NavBar from "./components/NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import Body from "./components/Body";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
-      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
